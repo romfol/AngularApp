0e01256ea43c9290bf62638790d65ed4dbb643ef
romfol <romanfoltyn1@gmail.com>
2019-12-15 23:00:39 +0200

    err handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,19 +3,22 @@ import { Hero } from "./hero";
 import { Observable, of } from "rxjs";
 import { MessageService } from "./message.service";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
 })
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(catchError(this.handleError<Hero[]>("getHeroes", [])));
   }
 
-  getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
-  }
+  // getHero(id: number): Observable<Hero> {
+  //   this.messageService.add(`HeroService: fetched hero id=${id}`);
+  //   return of(HEROES.find(hero => hero.id === id));
+  // }
 
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
